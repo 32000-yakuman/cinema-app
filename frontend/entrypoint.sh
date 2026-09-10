535f1a336508f93ef7bd3bf3cd231b185447d26e
usami_renko <hihu@club.com>
2026-09-10 19:39:00 +0900

    fix(dev) entrypointを開発専用に書き換え

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,11 +2,10 @@
 set -e
 
 if [ "$NODE_ENV" = "production" ]; then
-    echo "Starting in PRODUCTION mode (next build && next start)"
-    npm run build
+    echo "Starting in PRODUCTION mode"
     npm run start
 else
-    echo "Starting in DEVELOPMENT mode (next dev)"
+    echo "Starting in DEVELOPMENT mode"
 
     if [ ! -x /app/node_modules/.bin/next ]; then
         echo "node_modules is empty. Installing dependencies..."
