850000518db016542e3e18d65376be626767b8b0
usami_renko <hihu@club.com>
2026-09-11 09:23:46 +0900

    fix(frontend):ホームのURLを叩いたときにログインしなくても映画一覧画面に飛ぶように変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,20 +2,12 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import axios from "../plugins/axios";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    axios
-      .get("/api/cinema/me/")
-      .then(() => {
-        router.replace("/cinema/movies/");
-      })
-      .catch(() => {
-        router.replace("/login");
-      });
+    router.replace("/cinema/movies/");
   }, [router]);
 
   return (
